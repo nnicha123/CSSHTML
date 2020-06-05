49401656542e921dc2430e77f6f56c4ad8409e8a
Nicha Ngamtweerat <nicha_nga@hotmail.com>
2020-06-05 17:01:19 +0700

    added darkmode toggle

--- resume_dom/dom.js
+++ resume_dom/dom.js
@@ -16,18 +16,26 @@ function darkMode() {
     document.querySelector("button").style.backgroundColor = "whitesmoke";
     document.querySelector("button").style.color = "rgb(25, 39, 51)";
 }
-function inputItems(){
+function toggleDark() {
+    var element = document.body;
+    var newhead = document.getElementsByClassName("header")[0];
+    var socialEl = document.getElementsByClassName("social")[0];
+    socialEl.classList.toggle("social-dark-mode");
+    newhead.classList.toggle("header-dark-mode");
+    element.classList.toggle("dark-mode");
+}
+function inputItems() {
     let text = document.getElementById("experience-input").value;
     let el = document.createElement("p");
     el.innerHTML = text;
     let name;
-    if(document.getElementById("work1").selected){
+    if (document.getElementById("work1").selected) {
         name = "workOne";
-    }else if(document.getElementById("work2").selected){
+    } else if (document.getElementById("work2").selected) {
         name = "workTwo";
-    }else if(document.getElementById("work3").selected){
+    } else if (document.getElementById("work3").selected) {
         name = "workThree";
-    }else{
+    } else {
         name = "education";
     }
 
@@ -35,20 +43,20 @@ function inputItems(){
     document.getElementById("experience-input").value = "";
 }
 // Can remove skills by clicking on them
-function removeItem(id){
+function removeItem(id) {
     document.getElementById(id).style.display = "none";
 }
 // Change number via the input box
-function changeNumber(){
+function changeNumber() {
     let numberValue = document.getElementById("number-input").value;
     let name;
-    if(document.getElementById("Number").selected){
+    if (document.getElementById("Number").selected) {
         name = "number";
         document.getElementsByClassName("button-number")[0].innerHTML = "Click to update number";
-    }else if(document.getElementById("Email").selected){
+    } else if (document.getElementById("Email").selected) {
         name = "email";
         document.getElementsByClassName("button-number")[0].innerHTML = "Click to update email";
-    }else if(document.getElementById("Location").selected){
+    } else if (document.getElementById("Location").selected) {
         name = "location";
         document.getElementsByClassName("button-number")[0].innerHTML = "Click to update location";
     }

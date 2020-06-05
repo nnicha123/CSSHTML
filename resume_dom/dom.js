c77d81afb083e0e209a90fce7530dfef84b36284
Nicha Ngamtweerat <nicha_nga@hotmail.com>
2020-06-05 15:24:54 +0700

    can select which section to add input to

--- resume_dom/dom.js
+++ resume_dom/dom.js
@@ -1,8 +1,7 @@
 // h1 = document.createElement("h1");
 // h1.innerHTML = "hello";
 // document.body.appendChild(h1);
-document.querySelector("body").style.backgroundColor = rgb(25, 39, 51);
-document.getElementById("head").style.backgroundColor = "red";
+
 function darkMode() {
     document.body.style.backgroundColor = "rgb(25, 39, 51)";
     document.body.style.color = "white";
@@ -14,13 +13,25 @@ function darkMode() {
     document.getElementsByClassName("icon-top")[1].style.color = "rgb(25, 39, 51)";
     document.getElementsByClassName("icon-top")[2].style.color = "rgb(25, 39, 51)";
     document.getElementsByClassName("icon-top")[3].style.color = "rgb(25, 39, 51)";
-    document.getElementsByClassName("button-dark")[0].style.backgroundColor = "whitesmoke";
-    document.getElementsByClassName("button-dark")[0].style.color = "rgb(25, 39, 51)";
+    document.querySelector("button").style.backgroundColor = "whitesmoke";
+    document.querySelector("button").style.color = "rgb(25, 39, 51)";
 }
 function inputItems(){
     let text = document.getElementById("experience-input").value;
     let el = document.createElement("p");
     el.innerHTML = text;
-    document.getElementsByClassName("work-experience")[0].appendChild(el);
+    let name;
+    if(document.getElementById("work1").selected){
+        name = "workOne";
+    }else if(document.getElementById("work2").selected){
+        name = "workTwo";
+    }else if(document.getElementById("work3").selected){
+        name = "workThree";
+    }else{
+        name = "education";
+    }
+
+    document.getElementById(name).appendChild(el);
     document.getElementById("experience-input").value = "";
 }
+document.getElementsByClassName("skillset")[0].addEventListener("")

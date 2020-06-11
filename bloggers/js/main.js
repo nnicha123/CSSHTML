2a5fb6cdddd77a8139a2ab74104202acc20c0dc7
Nicha Ngamtweerat <nicha_nga@hotmail.com>
2020-06-11 11:46:31 +0700

    added more sections -> now at 2:23min

--- bloggers/js/main.js
+++ bloggers/js/main.js
@@ -10,7 +10,7 @@ $(document).ready(function(){
     // owl carousel for blog
     $('.owl-carousel').owlCarousel({
         loop:true,
-        autoplay:true,
+        autoplay:false,
         autoplayTimeout:3000,
         dots:false,
         nav:true,

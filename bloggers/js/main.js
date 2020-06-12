993ed32aed85f2b2d6eb3c81fe8113df10d89fae
Nicha Ngamtweerat <nicha_nga@hotmail.com>
2020-06-12 10:32:49 +0700

    added responsive behavior

--- bloggers/js/main.js
+++ bloggers/js/main.js
@@ -1,3 +1,15 @@
+const responsive = {
+    0 :{
+        items:1
+    },
+    520 :{
+        items:2
+    },
+    960 : {
+        items:3
+    }
+}
+
 $(document).ready(function(){
 
     $nav = $('.nav');
@@ -14,7 +26,15 @@ $(document).ready(function(){
         autoplayTimeout:3000,
         dots:false,
         nav:true,
-        navText:[$('.owl-navigation .owl-nav-prev'),$('.owl-navigation .owl-nav-next')]
+        navText:[$('.owl-navigation .owl-nav-prev'),$('.owl-navigation .owl-nav-next')],
+        responsive:responsive
     });
+
+    // click to scroll top
+    $('.move-up span').click(function(){
+        $('html,body').animate({
+            scrollTop:0
+        },3000);
+    })
 
 })

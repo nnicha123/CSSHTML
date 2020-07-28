04822f93ce133644a1b2b4f36aef46c2c35cd93c
Nicha Ngamtweerat <nicha_nga@hotmail.com>
2020-07-28 09:12:05 +0700

    fixed a few more bugs

--- resume_dom_no_bootstrap/dom.js
+++ resume_dom_no_bootstrap/dom.js
@@ -1,33 +1,12 @@
-// h1 = document.createElement("h1");
-// h1.innerHTML = "hello";
-// document.body.appendChild(h1);
-
-function darkMode() {
-  document.body.style.backgroundColor = 'rgb(25, 39, 51)';
-  document.body.style.color = 'white';
-  document.getElementById('head').style.backgroundColor = 'white';
-  document.getElementById('head').style.color = 'rgb(25, 39, 51)';
-  document.getElementsByClassName('social')[0].style.color = 'rgb(25, 39, 51)';
-  document.getElementsByClassName('social')[0].style.backgroundColor =
-    'whitesmoke';
-  document.getElementsByClassName('icon-top')[0].style.color =
-    'rgb(25, 39, 51)';
-  document.getElementsByClassName('icon-top')[1].style.color =
-    'rgb(25, 39, 51)';
-  document.getElementsByClassName('icon-top')[2].style.color =
-    'rgb(25, 39, 51)';
-  document.getElementsByClassName('icon-top')[3].style.color =
-    'rgb(25, 39, 51)';
-  document.querySelector('button').style.backgroundColor = 'whitesmoke';
-  document.querySelector('button').style.color = 'rgb(25, 39, 51)';
-}
 function toggleDark() {
   var element = document.body;
   var newhead = document.getElementsByClassName('header')[0];
   var socialEl = document.getElementsByClassName('social')[0];
+  var buttonToggle = document.getElementsByClassName('lightButton')[0];
   socialEl.classList.toggle('social-dark-mode');
   newhead.classList.toggle('header-dark-mode');
   element.classList.toggle('dark-mode');
+  buttonToggle.classList.toggle('lightButton-dark');
 }
 function inputItems() {
   let text = document.getElementById('experience-input').value;
@@ -77,7 +56,7 @@ function addSkills() {
   el.innerHTML = newSkill;
   el.setAttribute('class', 'skill-list');
   el.setAttribute('id', newSkill);
-  //   el.addEventListener('click', removeItem(id));
+  // el.addEventListener('click', removeItem(id));
   document.getElementById('allSkills').appendChild(el);
   document.getElementById('skill-input').value = '';
 }
